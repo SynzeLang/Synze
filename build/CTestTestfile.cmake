18373576b2b510cc12c095ce1dbf59d93db28a08
qloha <172928329+qloha@users.noreply.github.com>
2024-11-23 14:00:49 -0800

    gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/sirfi/Projects/Syntaxium
-# Build directory: C:/Users/sirfi/Projects/Syntaxium/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
